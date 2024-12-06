57e56c997267fe0e2efca81353415a4d59319357
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-06 11:24:24 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Armstrong{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=s.nextInt();
+            int temp=n;
+            int rev=0;
+            while(n>0)
+            {
+                int d=n%10;
+                rev=rev+d*d*d;
+                n/=10;
+            }
+            System.out.println(rev);
+        
+            if(temp==rev){
+                System.out.println("Armstrong");
+            }else{
+                System.out.println("not a Armstrong");
+            }
+        }
+    }
+}
